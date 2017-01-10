213de385eefd137c3317f186c484faafe1b0f8a2
Azharul Hoque <azharul-staffstack>
2017-01-10 19:28:19 +0600

    Project Structure Changed

---
+++ frontendServer/gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Azharul on 1/10/17.
+ */
+
+var gulp = require('gulp'),
+    runSequence = require('run-sequence');
+
+gulp.task('default', function(callback){
+    runSequence('build', callback);
+});
+
+gulp.task('build',function(callback){
+    runSequence('clean','copy-build',callback);
+});
+
+gulp.task('clean', function(){
+
+});
+
+gulp.task('copy-build', ['copy-assets', 'copy-app-js', 'copy-vendor-js']);
+
+gulp.task('copy-assets', function(){});
+gulp.task('copy-app-js', function(){});
+gulp.task('copy-vendor-js', function(){});
